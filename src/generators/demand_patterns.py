58bd5833259955a9e4961b3c077f7f6c6ba93850
Brad Haworth <289304793+bradhaworth-dev@users.noreply.github.com>
2026-05-31 17:18:46 -0500

    Add discontinued sku using np array

--- src/generators/demand_patterns.py
+++ src/generators/demand_patterns.py
@@ -22,14 +22,23 @@ def generate_seasonal(base_volume: int, num_weeks: int, amplitude: float, period
     seasonal_output = base_volume + amplitude * np.sin(2 * np.pi * np.arange(num_weeks)/period)
     return seasonal_output.round().astype(int)
 
+def generate_discontinued(base_volume: int, num_weeks: int, std_dev: float, discontinue_week: int, **kwargs) -> np.ndarray:
+    """
+    Takes a base volume with wobble and discontinues it at a given week.
+    """
+    discontinued_vol = (base_volume + (np.random.normal(0, std_dev, size = num_weeks))).round().astype(int)
+    discontinued_vol[discontinue_week:] = 0
+    return discontinued_vol
+
 PATTERN_GENERATORS = {
-    "steady":   generate_steady,
-    "trend":    generate_trend,
-    "seasonal": generate_seasonal,
+    "steady":       generate_steady,
+    "trend":        generate_trend,
+    "seasonal":     generate_seasonal,
+    "discontinued": generate_discontinued
 }
 
 ## Test ##
 if __name__ == "__main__":
-    param = {"base_volume": 500, "num_weeks": 104, "std_dev":5, "trend":-4.0, "amplitude": 150}
-    test = PATTERN_GENERATORS["seasonal"](**param)
+    param = {"base_volume": 500, "num_weeks": 104, "std_dev":5, "trend":-4.0, "amplitude": 150, "discontinue_week": 60}
+    test = PATTERN_GENERATORS["discontinued"](**param)
     print(test)
